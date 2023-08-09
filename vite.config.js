e7e4dcc2193db91108a605b354dc20ec941cc752
Albatrauz <michaelvg94@gmail.com>
2023-08-09 19:53:09 +0200

    Added vite config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ export default {
       external: /^(@firebase|firestore|firebase|phaser|lodash|react|react-dom)/, // Add the packages you want to exclude from bundling here
     },
   },
+  resolve: {
+    alias: {
+      'firebase/app': 'firebase/app',
+      'firebase/firestore': 'firebase/firestore', // Add this alias for Firestore
+    },
+  },
 };
